6c1e792f3558b9ee6c00426900c540552cf237b7
Neeraj Gupta <neeraj76@yahoo.com>
2021-12-25 11:40:53 +0530

    Restructured the code a bit to segragate old and new code

--- app/settings.py
+++ app/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 GOOGLE_OAUTH2_CLIENT_SECRETS_JSON = 'credentials.json'
