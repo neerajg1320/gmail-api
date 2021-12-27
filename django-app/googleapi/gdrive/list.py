ce85c317233f3fda4863f994c5758c4db6f524e2
Neeraj Gupta <neeraj76@yahoo.com>
2021-12-27 14:02:03 +0530

    Added the debug log to list_folder

--- django-app/googleapi/gdrive/list.py
+++ django-app/googleapi/gdrive/list.py
@@ -30,30 +30,31 @@ def list_files(credentials):
 
 # https://developers.google.com/drive/api/v3/search-files#python
 def list_folders(credentials):
-    print("list_folder(): called")
+    print("list_folders(): called")
     try:
         # Call the Gmail API
         service = build('drive', 'v3', credentials=credentials)
 
         # request a list of all the messages
-
-        response = service.files().list(
-            q="mimeType='application/vnd.google-apps.folder'",
-            fields='nextPageToken, files(id, name)'
-        ).execute()
-        files = response.get('files')
-
-        # nextPageToken = response.get('nextPageToken')
-        # while nextPageToken:
-        #     response = service.files().list().execute()
-        #     files.extend(response.get('files'))
-        #     nextPageToken = response.get('nextPageToken')
+        nextPageToken = None
+        files = []
+        while True:
+            response = service.files().list(
+                q="mimeType='application/vnd.google-apps.folder'",
+                fields='nextPageToken, files(id, name)'
+            ).execute()
+            files.extend(response.get('files'))
+            print("list_folders(): Total Files: {} nextPageToken={}".format(len(files), nextPageToken))
+            nextPageToken = response.get('nextPageToken', None)
+            if nextPageToken is None:
+                break
 
         if files is not None:
             print("list_files(): files={}".format(len(files)))
             # print("Total Files: {}".format(len(response.items)))
             for file in files:
                 print(file)
+
         return files
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
